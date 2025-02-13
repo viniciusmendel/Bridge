31bb1a2ac7c491de02dc9d47030376ece4a43164
viniciusmendel <vinyaps2710@gmail.com>
2025-02-13 09:49:51 -0600

    Ex. 2.1A

---
+++ Py serial arduino music.py
@@ -0,0 +1,19 @@
+import time
+
+import serial
+
+arduino = serial.Serial(port='COM5', baudrate=115200, timeout=10)
+
+
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+
+
+while True:
+    num = input("Enter a number: ")
+    value = write_read(num)
+    print(value)
+
